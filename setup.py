e1726f179b05a1b63fcdc95383305e0ee6be9c5e
David Gardenier <davidgardenier@gmail.com>
2020-02-20 15:02:40 +0100

    Update version numbering

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='frbcat',
-      version='0.1',
+      version='0.1.1',
       description='Query the FRB catalogue',
       url='http://github.com/davidgardenier/frbcat',
       author='David Gardenier',

effea45837e95331c8fdcca0436111ee6bcf2ce9
David Gardenier <davidgardenier@gmail.com>
2020-09-24 10:21:25 +0200

    Fixing relative imports

--- frbcat/__init__.py
+++ frbcat/__init__.py
@@ -1,4 +1,4 @@
 """Initialise pacakge."""
-from frbcat_file import Frbcat
-from chime_repeaters import ChimeRepeaters
-from tns import TNS
+from .frbcat_file import Frbcat
+from .chime_repeaters import ChimeRepeaters
+from .tns import TNS

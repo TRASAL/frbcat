ebb325889177e167da1077543a2690d9d0102587
Leon Oostrum <l.oostrum@esciencecenter.nl>
2021-06-11 15:12:54 +0200

    Fix pypi versioning (hopefully) and add README to description

--- setup.py
+++ setup.py
@@ -2,6 +2,11 @@
 import os
 from setuptools import setup
 
+
+with open('README.md') as f:
+    readme = f.read()
+
+
 with open(os.path.join('frbcat', '__version__.py')) as f:
     version = {}
     exec(f.read(), version)
@@ -11,6 +16,8 @@ with open(os.path.join('frbcat', '__version__.py')) as f:
 setup(name='frbcat',
       version=project_version,
       description='Query the FRB catalogue',
+      long_description=readme,
+      long_description_content_type='text/markdown',
       url='http://github.com/davidgardenier/frbcat',
       author='David Gardenier',
       author_email='davidgardenier@gmail.com',
